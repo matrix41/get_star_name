bce1b61cc0d67c58f8079788f6e7ab35c94aea77
matrix41 <raymond@vivaldi.ipac.caltech.edu>
2014-12-10 08:57:47 -0800

    removes extra space in HD/HIP name; strips out leading zeros; inserts a space in star name (ie HD123 becomes HD 123); read only first three lines of file and ignore the rest (thereby making it run faster)

--- get_star_name.pl
+++ get_star_name.pl
@@ -8,6 +8,8 @@ use Tie::IxHash;
 my $s1;
 my $s1b;
 my $filename = $ARGV[0];
+my $thing1;
+my $thing2;
 chomp $filename;
 
 # Declare new filehandle and associated it with filename
@@ -15,19 +17,28 @@ open (my $fh, '<', $filename) or die "\nCould not open file '$filename' $!\n";
 my @array = <$fh>;
 close ($fh);
 
-for ( my $i = 0 ; $i <= $#array ; $i++ )
+for ( my $i = 0 ; $i <= 3 ; $i++ )
 {
 	if ( $array[$i] =~ /Star_name/ )
 	{
 #	    print $array[$i];
-        my @parts = split(/=/, $array[$i]);
-	    my $wholeText = $parts[1];
+        my @parts = split(/=/, $array[$i]); # split string on the equal sign 
+	    my $wholeText = $parts[1]; # get the contents of the right side of the equal sign 
 		if ($wholeText =~ /\"$/)   #check the last character if " which is the end of the string
 		{
 		   $wholeText =~ s/\"(.*)\"/$1/;   #extract the string, removed the quotes
 		}
-		print "$filename  $wholeText";
+#		print "$filename  $wholeText";
+
+        if ( $wholeText =~ /\D+\d+/ )
+        {
+            $wholeText =~ s/(\D+)(\d+)/$1$2/;
+            $thing1 = $1;
+            $thing2 = $2;
+        }
+
+        $thing1 =~ s/\s+$//; # this line gets rid of space character in HD or HIP 
+        $thing2 =~ s/^0+//g; # this line gets rid of leading zeros 
+		print "$filename  $thing1 $thing2\n";
 	}
 }
-
-
